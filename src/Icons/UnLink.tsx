a71c8a73680e8b677ca66bbb76b2c8d1cf2b5d59
Danushka Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2024-07-01 21:26:14 +0530

    feat: create UnLink.tsx

---
+++ src/Icons/UnLink.tsx
@@ -0,0 +1,7 @@
+const UnLink = ({ size }: { size: number }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg"  width={size} height={size} className='text-default' viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M9 17H7A5 5 0 0 1 7 7"/><path d="M15 7h2a5 5 0 0 1 4 8"/><line x1="8" x2="12" y1="12" y2="12"/><line x1="2" x2="22" y1="2" y2="22"/></svg>
+  )
+}
+
+export default UnLink
