f1a3835376b0fcc17d4136b6a634407d2681ccc2
Danushka Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2024-07-01 21:42:00 +0530

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import { NextUIProvider } from '@nextui-org/react'
+import { Toaster } from 'react-hot-toast';
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <NextUIProvider>
+    <Toaster/>
     <App />
   </NextUIProvider>
 )
