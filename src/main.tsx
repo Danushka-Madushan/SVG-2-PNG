ef8e0a01aca2f74c86e95bd71ff30ea0d45df682
Danushka Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2024-07-01 21:26:07 +0530

    feat: create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+import { NextUIProvider } from '@nextui-org/react'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <NextUIProvider>
+    <App />
+  </NextUIProvider>
+)
