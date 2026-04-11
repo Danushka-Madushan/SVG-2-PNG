2ed36e3931dd6e728c9a1cbea71e08d271c61cdc
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-04-11 11:09:32 +0530

    update vite.config.github.js

--- vite.config.github.js
+++ vite.config.github.js
@@ -6,8 +6,12 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+ plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: '/SVG-2-PNG'
 })
